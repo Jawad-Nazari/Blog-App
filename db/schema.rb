111609d3a0b62be7a0ae7a797d2cf0a373a70a2d
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-07-19 13:08:09 +0500

    Migrate the comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_19_075419) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_19_080136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "author_id", null: false
@@ -34,5 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_19_075419) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
