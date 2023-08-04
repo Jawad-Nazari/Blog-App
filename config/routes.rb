d960f5f18768b29dda933019e84fa51a138a952b
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-08-04 12:03:27 +0500

    Setup routes for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,25 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
+
   
-  root 'users#index'
-resources :users, only: [:index, :show] do
-  resources :posts, only: [:index, :show, :new, :create, :destroy] do
-    resources :comments, only: [:new, :create, :destroy]
-    resources :likes, only: [:create]
+  root to: 'users#index'
+
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:new, :create, :destroy]
+      resources :likes, only: [:create]
+    end
   end
-end
+
+  # Resources for the API
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
 end
